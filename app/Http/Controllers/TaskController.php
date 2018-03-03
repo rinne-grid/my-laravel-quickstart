2ee9d062a6bb15658d3b820d1d6a170558e83f8b
rinne-grid <Rinne.Grids@gmail.com>
2018-03-03 16:18:55 +0900

    追加 バリデーション

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class TaskController extends Controller
+{
+    //
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function index(Request $request) {
+        return view('tasks.index');
+    }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:20',
+        ]);
+    }
+}
